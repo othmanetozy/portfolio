123a464f44f0eba928d1eb954309031a9e1c7ac0
othmane tozy <tozyothmane123@gmail.com>
2024-04-25 16:21:48 +0100

    first commit

---
+++ src/components/3-main/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import './main.css';
+
+
+const Main = () =>{
+    return(
+        <div>
+            HEADER   
+        </div>
+    );
+}
+
+
+export default Main;
---
+++ src/components/5-footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './footer.css';
+const footer = () =>{
+    return(
+        <div>
+            Hero   
+        </div>
+    );
+}
+
+
+export default footer;
